3d2fe7dab6c2ea8423d3857964cf027fbdb9851c
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-07-16 10:23:01 +0200

    get the DOM elements

---
+++ main.js
@@ -0,0 +1,8 @@
+const currencyOne = document.getElementById("currency-one");
+const amountOne = document.getElementById("amount-one");
+
+const currencyTwo = document.getElementById("currency-two");
+const amountTwo = document.getElementById("amount-two");
+
+const swapBtn = document.getElementById("swap");
+const rateDiv = document.getElementById("rate");
